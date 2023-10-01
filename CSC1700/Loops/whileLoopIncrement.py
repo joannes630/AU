f4e4864a08e360e99d6ccc706e8720a998b0b173
joannes630 <130481030+joannes630@users.noreply.github.com>
2023-10-01 12:34:40 -0500

    Update whileLoopIncrement.py

--- CSC1700/Loops/whileLoopIncrement.py
+++ CSC1700/Loops/whileLoopIncrement.py
@@ -1,6 +1,18 @@
 # This code shows how to increment an integer
 # inside a while loop and display it.
 
+# This will display
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
 count = 1
 while count <= 10:
     print(count)
