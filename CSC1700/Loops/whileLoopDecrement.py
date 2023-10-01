a448c86906ac4c7ee577c136b866969962ed31d9
joannes630 <130481030+joannes630@users.noreply.github.com>
2023-10-01 12:34:00 -0500

    Update whileLoopDecrement.py

--- CSC1700/Loops/whileLoopDecrement.py
+++ CSC1700/Loops/whileLoopDecrement.py
@@ -1,6 +1,18 @@
 # This code shows how to decrement an integer
 # inside a while loop and display it.
 
+# This will display
+# 10
+# 9
+# 8
+# 7
+# 6
+# 5
+# 4
+# 3
+# 2
+# 1
+
 count = 10
 while count > 0:
     print(count)
