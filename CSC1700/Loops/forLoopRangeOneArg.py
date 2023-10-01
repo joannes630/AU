7426323c518ebed387fd06cfbdccabdc64b0b00d
joannes630 <130481030+joannes630@users.noreply.github.com>
2023-10-01 12:30:22 -0500

    Update forLoopRangeOneArg.py

--- CSC1700/Loops/forLoopRangeOneArg.py
+++ CSC1700/Loops/forLoopRangeOneArg.py
@@ -1,5 +1,18 @@
 # This code shows how to use the range function
-# with only one argument. 
+# with only one argument.
+
+# This will display
+# 0
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
 
 for number in range(10):
     print(number)
+      
