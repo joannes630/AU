ed77aaa9acc9704c8a60033304d2f13b6e4c3580
joannes630 <130481030+joannes630@users.noreply.github.com>
2023-10-01 12:41:47 -0500

    Update forLoopUseAList.py

--- CSC1700/Loops/forLoopUseAList.py
+++ CSC1700/Loops/forLoopUseAList.py
@@ -1,5 +1,10 @@
 # This code shows how to use a list of item
 # with a for loop to iterate through the items
 
+# This will display
+# Steve
+# Tony
+# Bruce
+
 for name in ["Steve", "Tony", "Bruce"]:
     print(name)
