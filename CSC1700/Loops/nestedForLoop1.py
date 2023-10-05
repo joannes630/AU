118ad00db40a5831e84b5cbbae5fc8db15edd51b
joannes630 <130481030+joannes630@users.noreply.github.com>
2023-10-05 08:43:51 -0500

    Update nestedForLoop1.py

--- CSC1700/Loops/nestedForLoop1.py
+++ CSC1700/Loops/nestedForLoop1.py
@@ -1,1 +1,14 @@
+# This code is using a nested for loop to display
+# ripe banana
+# ripe apple
+# ripe orange
+# fresh banana
+# fresh apple
+# fresh orange
+# delicious banana
+# delicious apple
+# delicious orange
 
+for adj in ["ripe", "fresh", "delicious"]:
+    for fruits in ["banana", "apple", "orange"]:
+        print(adj, fruits)
