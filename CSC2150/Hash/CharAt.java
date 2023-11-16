81250685ea15b23c844aca016c194d0f7b097567
joannes630 <jreyes@aurora.edu>
2023-11-16 12:48:42 -0600

    Updates

---
+++ CSC2150/Hash/CharAt.java
@@ -0,0 +1,32 @@
+public class CharAt {
+
+	static int hashCode(String  name) {
+		int base=2;
+		int hash = 0;
+		for (int i = 0; i < name.length(); i++) {
+			hash = (hash * base) + name.charAt(i);
+			System.out.println(hash);
+		}
+
+//		hash = Math.abs(hash);
+		hash %= 10;
+		return hash;
+	}
+
+	public static void main(String[] args) {
+		String name = "Walter Smith";
+//		System.out.println((int) name.charAt(0));
+//		System.out.println((int) name.charAt(1));
+//		System.out.println((int) name.charAt(2));
+//		System.out.println((int) name.charAt(3));
+//		System.out.println((int) name.charAt(4));
+//		System.out.println((int) name.charAt(5));
+
+//		for (int i=0; i<name.length(); i++)
+//			System.out.println((int) name.charAt(i));
+
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(hashCode(name));
+
+	}
+}
--- CSC2150/Hash/_C_HashSetCollission.java
+++ CSC2150/Hash/_C_HashSetCollission.java
@@ -15,82 +15,85 @@ public class _C_HashSetCollission {
     }
 
     static void searchArray(Scanner keyboard, int grades[]) {
-        System.out.print("Enter grade to search: ");
+        System.out.print("Enter grade to search (-1 to end): ");
         int grade = keyboard.nextInt();
         while (grade != -1) {
             boolean found = false;
             int hash = hashCode(grade);
-            if (grade == grades[hash])
-                found = true;
-            else {
-                while ((hash < grades.length) && grades[hash] != -1) {
-                    if (grade == grades[hash]) {
-                        found = true;
-                        break;
-                    }
-                    hash++;
+            while (grades[hash] != -1 && !found) {
+                if (grade == grades[hash]) {
+                    found = true;
                 }
+                hash++;
+                if (hash >= grades.length)
+                    hash = 0;
+
             }
             if (found)
                 System.out.println("The number " + grade + " is in the array.");
             else
                 System.out.println("The number " + grade + " is not in the array.");
-            System.out.print("Enter number: ");
+            System.out.print("Enter grade to search (-1 to end): ");
             grade = keyboard.nextInt();
         }
+        System.out.println();
     }
 
     static void removeGrade(Scanner keyboard, int grades[]) {
-        System.out.print("Enter grade to remove: ");
+        System.out.print("Enter grade to remove (-1 to end): ");
         int grade = keyboard.nextInt();
         while (grade != -1) {
             boolean found = false;
             int hash = hashCode(grade);
-            if (grade == grades[hash])
-                found = true;
-            else {
-                hash++;
-                while ((hash < grades.length) && grades[hash] != -1) {
-                    if (grade == grades[hash]) {
-                        found = true;
-                        break;
-                    }
+            while (grades[hash] != -1 && !found) {
+                if (grade == grades[hash]) {
+                    found = true;
                 }
+                if (!found)
+                    hash++;
+                if (hash >= grades.length)
+                    hash = 0;
             }
+
             if (found)
                 grades[hash] = -2;
             else
                 System.out.println("The number " + grade + " is not in the array.");
             printArray(grades);
-            System.out.print("Enter number: ");
+            System.out.print("Enter grade to remove (-1 to end): ");
             grade = keyboard.nextInt();
         }
+        System.out.println();
     }
 
     // -1 means this spot has never been used
     // -2 means this spot was used and removed
     static boolean checkForDuplicates(int grade, int grades[], int hash) {
-        while ((hash < grades.length) && grades[hash] != -1) {
+        System.out.println(hash);
+        while (grades[hash] != -1) {
             if (grade == grades[hash]) {
                 System.out.println("Error: Cannot add duplicate item " + grade);
                 return true;
             }
             hash++;
+            if (hash >= grades.length)
+                hash = 0;
         }
         return false;
     }
     static void add(int grade, int grades[]) {
         int hash = hashCode(grade);
+        System.out.println(hash);
         boolean duplicate = checkForDuplicates(grade, grades, hash);
 
         if (!duplicate) {
-            while ((hash < grades.length) && (grades[hash] != -1 && grades[hash] != -2)) {
+            while (grades[hash] != -1 && grades[hash] != -2) {
                 hash++;
+                if (hash >= grades.length)
+                    hash = 0;
             }
-            if (hash < grades.length)
-                grades[hash] = grade;
-            else
-                System.out.println("Error: Out of Bounds, Increase hash table.");
+
+            grades[hash] = grade;
         }
     }
 
@@ -104,7 +107,6 @@ public class _C_HashSetCollission {
         // Populate our hash table
         add(97, grades);
         add(82, grades);
-        add(0, grades);
         // Make it hash to the same index 2
         add(92, grades);
         add(72, grades);
@@ -112,6 +114,8 @@ public class _C_HashSetCollission {
         add(83, grades);
         // Try populate with a duplicate item
         add(83, grades);
+        add(99, grades);
+        add(89, grades);
 
         // Perform operations
         Scanner keyboard = new Scanner(System.in);
