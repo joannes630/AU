db2f89083e94092ef13f9808f8699b43813fe82f
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-03-25 07:44:48 -0500

    Update index_list.py

--- CSC1700/Lists and Tuples/index_list.py
+++ CSC1700/Lists and Tuples/index_list.py
@@ -1,34 +1,13 @@
-# This program demonstrates how to get the
-# index of an item in a list and then replace
-# that item with a new item.
-
 def main():
-    # Create a list with some items.
-    food = ['Pizza', 'Burgers', 'Chips']
-
-    # Display the list.
-    print('Here are the items in the food list:')
-    print(food)
-
-    # Get the item to change.
-    item = input('Which item should I change? ')
-
-    try:
-        # Get the item's index in the list.
-        item_index = food.index(item)
-
-        # Get the value to replace it with.
-        new_item = input('Enter the new value: ')
-
-        # Replace the old item with the new item.
-        food[item_index] = new_item
-
-        # Display the list.
-        print('Here is the revised list:')
-        print(food)
-    except ValueError:
-        print('That item was not found in the list.')
-
+    my_list = ["Walter", "Jesse", "Saul", "Mike"]
+    search = input("Enter name: ")
+    if search in my_list:
+        index = my_list.index(search)
+        my_list[index] = "Gustavo"
+    else:
+        print(f"{search} is not in the list")
+    print(my_list)
+    
 # Call the main function.
 if __name__ == '__main__':
     main()
