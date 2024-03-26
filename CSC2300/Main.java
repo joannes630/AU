a06be5611c8ecfbded427db978e8be82bfa7a50d
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-03-25 19:02:30 -0500

    Add files via upload

---
+++ CSC2300/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args)
+    {
+        final double tolerance = 1e-5;
+        float x = 36.1f;
+        double y = 36.1;
+        System.out.println(y - x);
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.MIN_NORMAL);
+        
+
+//        int a = 2_147_483_647;
+//        float b = 2.147483647E9F;
+//        int c = a - (int) b;
+//        System.out.println(c);
+
+//        System.out.println(x - y);
+//        if (Math.abs(x-y) < tolerance) {
+//            System.out.println("x and y are the same values");
+//        }
+//        else {
+//            System.out.println("x and y are not the same values");
+//        }
+    }
+}
+
