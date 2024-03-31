c3f6ad3e0f7ee49099c178ce1a29d4dd16454b37
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-03-31 10:27:27 -0500

    Update write_number_list.py

--- CSC1700/Lists and Tuples/write_number_list.py
+++ CSC1700/Lists and Tuples/write_number_list.py
@@ -9,3 +9,32 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+"""
+The for loop is
+for item in numbers:
+for item in [1, 2, 3, 4, 5, 6, 7]:
+
+outfile.write(str(1) + "\n")
+File: "1\n"
+
+outfile.write(str(2) + "\n")
+File: "1\n2\n"
+
+outfile.write(str(3) + "\n")
+File: "1\n2\n3\n"
+
+and so on, until finally
+
+outfile.write(str(7) + "\n")
+File: "1\n2\n3\n4\n5\n6\n7\n"
+
+After the program runs, the file would contain
+1
+2
+3
+4
+5
+6
+7
+"""
