b9cf39ce9bac4362ee031b43166c46588b4bd9b1
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-04-01 08:05:09 -0500

    Update readlines.py

--- CSC1700/Lists and Tuples/readlines.py
+++ CSC1700/Lists and Tuples/readlines.py
@@ -19,18 +19,18 @@ def main():
 main()
 
 """
-list = ["1.2", "3.4", "5.6"]
+list = ["1.2\n", "3.4\n", "5.6\n"]
 
-line = "1.2"
-total = 0 + float("1.2") = 1.2
+line = "1.2\n"
+total = 0 + float("1.2\n") = 1.2
 count = 0 + 1 = 1
 
-line = "3.4"
-total = 1.2 + float("3.4") = 4.6
+line = "3.4\n"
+total = 1.2 + float("3.4\n") = 4.6
 count = 1 + 1 = 2
 
-line = "5.6"
-total = 4.6 + float("5.6") = 10.2
+line = "5.6\n"
+total = 4.6 + float("5.6\n") = 10.2
 count = 2 + 1 = 3
 
 avg = 10.2 / 3 = 3.4
