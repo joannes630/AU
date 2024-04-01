7682223c5287d6eaae77052fc3a46753d7bcb3bb
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-04-01 08:19:20 -0500

    Update 2d_list.py

--- CSC1700/Lists and Tuples/2d_list.py
+++ CSC1700/Lists and Tuples/2d_list.py
@@ -9,3 +9,24 @@ def main():
             print(item)
 
 main()
+
+"""
+In the first iteration of the outer loop, the variable row will contain
+row = ['Joe', 'Kim']
+This row is passed in the inner loop. Now in each iteration of the inner
+the variable item will contain
+'Joe'
+'Kim'
+
+And each time it would print it.
+Ans so on. In the second iteration of the outer loop
+row = ['Sam', 'Sue']
+item = 'Sam'
+item = 'Sue'
+
+And finally the third iteration of the outer loop
+row = ['Kelly', 'Chris']
+item = 'Kelly'
+item = 'Chris'
+
+"""
