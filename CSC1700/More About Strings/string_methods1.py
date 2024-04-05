a3d97238ad077e47b9965e862f7770df6d41f990
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-04-04 22:22:32 -0500

    Create string_methods1.py

---
+++ CSC1700/More About Strings/string_methods1.py
@@ -0,0 +1,41 @@
+def main():
+    str = "alphabet"
+    print(f'For the string \'{str}\', the following are the return values: ')
+    print(f'isalnum(): {str.isalnum()}')
+    print(f'isalpha(): {str.isalpha()}')
+    print(f'isdigit(): {str.isdigit()}')
+    print(f'islower(): {str.islower()}')
+    print(f'isspace(): {str.isspace()}')
+    print(f'isupper(): {str.isupper()}')
+    print()
+
+    str = "ALPHABET123"
+    print(f'For the string \'{str}\', the following are the return values: ')
+    print(f'isalnum(): {str.isalnum()}')
+    print(f'isalpha(): {str.isalpha()}')
+    print(f'isdigit(): {str.isdigit()}')
+    print(f'islower(): {str.islower()}')
+    print(f'isspace(): {str.isspace()}')
+    print(f'isupper(): {str.isupper()}')
+    print()
+
+    str = "0123456"
+    print(f'For the string \'{str}\', the following are the return values: ')
+    print(f'isalnum(): {str.isalnum()}')
+    print(f'isalpha(): {str.isalpha()}')
+    print(f'isdigit(): {str.isdigit()}')
+    print(f'islower(): {str.islower()}')
+    print(f'isspace(): {str.isspace()}')
+    print(f'isupper(): {str.isupper()}')
+    print()
+
+    str = ' \t \n'
+    print(f'For the string \'{str}\', the following are the return values: ')
+    print(f'isalnum(): {str.isalnum()}')
+    print(f'isalpha(): {str.isalpha()}')
+    print(f'isdigit(): {str.isdigit()}')
+    print(f'islower(): {str.islower()}')
+    print(f'isspace(): {str.isspace()}')
+    print(f'isupper(): {str.isupper()}')
+
+main()
