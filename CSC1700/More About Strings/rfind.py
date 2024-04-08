b7e0d83faed21e5db2c94b2eecf6bed06e8688d0
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-04-08 08:51:38 -0500

    Update rfind.py

--- CSC1700/More About Strings/rfind.py
+++ CSC1700/More About Strings/rfind.py
@@ -19,9 +19,14 @@ def main():
     last name since there is a middle name. One solution is use the method
     rfind(), to start the search from the end (rear) of the string.
     '''
-    idx = name.rfind(' ')
+    idx = name.find(' ', idx+1)
     last = name[idx+1:]
     print(last)
 
+    # What about if the name if 'John Ronald Reuel Tolkien'?
+    '''
+    0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
+    J  o  h  n     R  o  n  a  l  d     R  e  u  e  l     T  o  l  k  i  e  n 
+    '''
 main()
 
