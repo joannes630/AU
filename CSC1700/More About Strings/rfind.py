d2550206ab78e3876f405b18407eeb9112438abd
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-04-08 08:53:02 -0500

    Update rfind.py

--- CSC1700/More About Strings/rfind.py
+++ CSC1700/More About Strings/rfind.py
@@ -23,7 +23,7 @@ def main():
     last = name[idx+1:]
     print(last)
 
-    # What about if the name if 'John Ronald Reuel Tolkien'?
+    # What about if the name is 'John Ronald Reuel Tolkien'?
     '''
     0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15 16 17 18 19 20 21 22 23 24
     J  o  h  n     R  o  n  a  l  d     R  e  u  e  l     T  o  l  k  i  e  n 
