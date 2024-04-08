f2d53f81bafcb0dcebb679bd315e5e554c881ccb
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-04-08 08:45:02 -0500

    Create rfind.py

---
+++ CSC1700/More About Strings/rfind.py
@@ -0,0 +1,27 @@
+def main():
+    '''
+    Assume the string name is composed of first, middle, and last.
+    '''
+    name = "Walter E. Smithe"
+
+    '''
+    index =  0  1  2  3  4  5  6  7  8  9  10 11 12 13 14 15
+    string = W  a  l  t  e  r     E  .     S  m  i  t  h  e
+    '''
+    # Get the first name
+    idx = name.find(' ')    # This will return 6
+    first = name[:idx]      # We are basically stating to slice from
+                            # index 0, up to but not including index 6
+    print(first)
+
+    '''
+    Now, we could not use the current value of idx anymore to get the 
+    last name since there is a middle name. One solution is use the method
+    rfind(), to start the search from the end (rear) of the string.
+    '''
+    idx = name.rfind(' ')
+    last = name[idx+1:]
+    print(last)
+
+main()
+
