d5497e62bbd7f5d8c2aacc3a42ab69079be036ac
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-04-17 08:14:53 -0500

    Create passing_arguments_to_a_function2.py

---
+++ CSC1700/Cumulative_Review/passing_arguments_to_a_function2.py
@@ -0,0 +1,23 @@
+def search_value(dict, search):
+    found = False
+    for key, value in dict.items():
+        for x in value:
+            if search == x:
+                print("The number is found in key", key)
+                found = True
+    return found
+
+def main():
+    # Dictionary
+    dict = {'Ross': [1, 2, 3],
+            'Joey': [4, 5, 6],
+            'Chandler': [7, 8, 9]}
+
+    # Search using value in a dictionary
+    search = int(input("Enter number to search: "))
+    found = search_value(dict, search)
+
+    if not found:
+        print("The number was not found")
+
+main()
