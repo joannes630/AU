03df7cc230deeda767f195986fdf4d91c3a8db31
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-04-17 07:43:36 -0500

    Update total_without_index.py

--- CSC1700/Cumulative_Review/total_without_index.py
+++ CSC1700/Cumulative_Review/total_without_index.py
@@ -1,27 +1,30 @@
-# Calculating running total without using an index
-# One Dimensional List
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-total = 0
-for x in my_list:
-    total += x
-print(total)
-
-# Two Dimensional List
-my_2dlist = [[1, 2, 3],
-             [4, 5, 6],
-             [7, 8, 9]]
-total = 0
-for row in my_2dlist:
-    for col in row:
-        total += col
-print(total)
-
-# Dictionary
-dict = {'Ross': [1, 2, 3],
-        'Joey': [4, 5, 6],
-        'Chandler': [7, 8, 9]}
-total = 0
-for key, value in dict.items():
-    for x in value:
+def main():
+    # Calculating running total without using an index
+    # One Dimensional List
+    my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    total = 0
+    for x in my_list:
         total += x
-print(total)
+    print(total)
+    
+    # Two Dimensional List
+    my_2dlist = [[1, 2, 3],
+                 [4, 5, 6],
+                 [7, 8, 9]]
+    total = 0
+    for row in my_2dlist:
+        for col in row:
+            total += col
+    print(total)
+    
+    # Dictionary
+    dict = {'Ross': [1, 2, 3],
+            'Joey': [4, 5, 6],
+            'Chandler': [7, 8, 9]}
+    total = 0
+    for key, value in dict.items():
+        for x in value:
+            total += x
+    print(total)
+    
+main()
