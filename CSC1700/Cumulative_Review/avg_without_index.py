9553355f1bf0cb8d87a123d92e0cbcad4b7e78f2
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-04-16 22:30:20 -0500

    Create avg_without_index.py

---
+++ CSC1700/Cumulative_Review/avg_without_index.py
@@ -0,0 +1,35 @@
+# Calculating average using an index
+# One Dimensional List
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+total = count = 0
+for i in range(len(my_list)):
+    total += my_list[i]
+    count += 1
+avg = total / count
+print(avg)
+
+# Average of all the elements of a 2-D list
+# Two Dimensional List
+my_2dlist = [[1, 2, 3],
+             [4, 5, 6],
+             [7, 8, 9]]
+total = count = 0
+for i in range(len(my_2dlist)):
+    for j in range(len(my_2dlist[i])):
+        total += my_2dlist[i][j]
+        count += 1
+avg = total / count
+print(avg)
+
+# Average of each row of a 2-D list
+# Two Dimensional List
+my_2dlist = [[1, 2, 3],
+             [4, 5, 6],
+             [7, 8, 9]]
+for i in range(len(my_2dlist)):
+    total = count = 0
+    for j in range(len(my_2dlist[i])):
+        total += my_2dlist[i][j]
+        count += 1
+    avg = total / count
+    print(avg)
