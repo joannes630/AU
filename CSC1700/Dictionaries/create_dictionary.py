19e35a5175f19c1d2f8852ed086e81fb50a044a4
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-04-16 22:12:21 -0500

    Create create_dictionary.py

---
+++ CSC1700/Dictionaries/create_dictionary.py
@@ -0,0 +1,18 @@
+'''
+Write a dictionary with names as the key and the phone number as values.
+Both keys and values are strings. Display the dictionary.
+
+Name = John
+Phone number = 847-234-5678
+Name = Walter
+Phone number = 224-123-5678
+Name = Peter
+Phone number = 630-456-7890
+'''
+
+def main():
+    phonebook = {'John':'847-234-5678', 'Walter':'224-123-5678',
+                 'Peter':'630-456-7890'}
+    print(phonebook)
+
+main()
