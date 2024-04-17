36240ec7285e9844f9c1c71444eccce68baf824d
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-04-16 22:15:48 -0500

    Create empty_dictionary.py

---
+++ CSC1700/Dictionaries/empty_dictionary.py
@@ -0,0 +1,21 @@
+'''
+An empty dictionary is created for you in the program below.
+Add the following key/value pairs to the dictionary and display it.
+Both key and value are strings.
+
+Name = John
+Phone number = 847-234-5678
+Name = Walter
+Phone number = 224-123-5678
+Name = Peter
+Phone number = 630-456-7890
+'''
+
+def main():
+    phonebook = {}
+    phonebook['John'] = '847-234-5678'
+    phonebook['Walter'] = '224-123-5678'
+    phonebook['Peter'] = '630-456-7890'
+    print(phonebook)
+
+main()
