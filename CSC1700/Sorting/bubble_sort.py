abe4fc7656c41ef08517b8b1abe96bb741f8359f
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-04-19 08:24:14 -0500

    Update bubble_sort.py

--- CSC1700/Sorting/bubble_sort.py
+++ CSC1700/Sorting/bubble_sort.py
@@ -1,8 +1,8 @@
 def bubble_sort(arr):
     n = len(arr)
-    # Traverse through all elements in the array
+    # Traverse through all elements in the list
     for i in range(n):
-        # Traverse through the array, excluding the already sorted elements at the end
+        # Traverse through the list, excluding the already sorted elements at the end
         for j in range(n - i - 1):
             # Compare adjacent elements and swap them if needed
             if arr[j] > arr[j + 1]:
@@ -11,6 +11,9 @@ def bubble_sort(arr):
 def main():
     arr = [64, 34, 25, 12, 22, 11, 9]
     print("Original array:", arr)
+
+    # Notice below that we did not need to receive a return value of the list when calling bubble_sort function.
+    # It is because the list is mutable. We are changing the list itself in the function.
     bubble_sort(arr)
     print("Sorted array:", arr)
 
