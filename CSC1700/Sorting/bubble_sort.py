769a18a7c46d489d734853a9ce0c7f99b100e412
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-04-19 08:24:34 -0500

    Update bubble_sort.py

--- CSC1700/Sorting/bubble_sort.py
+++ CSC1700/Sorting/bubble_sort.py
@@ -12,7 +12,7 @@ def main():
     arr = [64, 34, 25, 12, 22, 11, 9]
     print("Original array:", arr)
 
-    # Notice below that we did not need to receive a return value of the list when calling bubble_sort function.
+    # Notice below that we do not need to receive a return value of the list when calling bubble_sort function.
     # It is because the list is mutable. We are changing the list itself in the function.
     bubble_sort(arr)
     print("Sorted array:", arr)
