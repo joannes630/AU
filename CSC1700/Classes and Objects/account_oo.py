a6476788b9752eefb72c1318a444e045e6941fc9
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-04-22 08:19:15 -0500

    Update account_oo.py

--- CSC1700/Classes and Objects/account_oo.py
+++ CSC1700/Classes and Objects/account_oo.py
@@ -10,7 +10,10 @@ class Account:
         print(f"Address: {self.addr}")
 
 def main():
-    account = Account("John", "123-4567", "10 Maple St.")
-    account.display_account()
+    account1 = Account("John", "123-4567", "10 Maple St.")
+    account1.display_account()
+
+    account2 = Account("Tim", "234-5678", "10 Maple St.")
+    account2.display_account()
 
 main()
