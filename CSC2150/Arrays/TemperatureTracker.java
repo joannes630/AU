e8e2cdbb4479379a6076cfd95f20915018b60f87
joannes630 <jreyes@aurora.edu>
2024-09-27 12:03:39 -0500

    Update TemperatureTracker.java

--- CSC2150/Arrays/TemperatureTracker.java
+++ CSC2150/Arrays/TemperatureTracker.java
@@ -1,31 +1,31 @@
-public class TemperatureTracker {
+public class Temperature {
+    // Write your code here
     public static void main(String[] args) {
-        // 1. Create an array to store temperatures for 7 days
-        int[] temperatures = {23, 27, 21, 25, 30, 26, 22};
+        int[] temp = {23, 27, 21, 25, 30, 26, 22};
 
-        // 2. Find the highest temperature
-        int highest = temperatures[0];
-        for (int temp : temperatures) {
-            if (temp > highest) {
-                highest = temp;
-            }
+        int highest = temp[0];
+        for (int i=0; i<temp.length; i++) {
+            if (temp[i] > highest)
+                highest = temp[i];
         }
+
+        int lowest = temp[0];
+        for (int i=0; i<temp.length; i++) {
+            if (temp[i] < lowest)
+                lowest = temp[i];
+        }
+
+        int total = 0;
+        for (int i=0; i<temp.length; i++) {
+            total += temp[i];
+        }
+        double avg = (double) total / temp.length;
+
         System.out.println("The highest temperature is " + highest);
-
-        // 3. Find the lowest temperature
-        int lowest = temperatures[0];
-        for (int temp : temperatures) {
-            if (temp < highest) {
-                lowest = temp;
-            }
-        }
         System.out.println("The lowest temperature is " + lowest);
-
-        // 4. Find the average temperature
-        int sum = 0;
-        for (int temp : temperatures)
-            sum += temp;
-        double avg = (double) sum / temperatures.length;
-        System.out.println("The average temperature is " + String.format("%.2f", avg));             
+        System.out.println("The average temperature is " + avg);
     }
 }
+
+
+
