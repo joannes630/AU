0a848f09dae4ec4ec2dacb37684c8568ed8dfaf0
joannes630 <jreyes@aurora.edu>
2024-11-04 09:04:03 -0600

    Create review.py

---
+++ CSC1700/List/review.py
@@ -0,0 +1,41 @@
+"""
+1. The following function should accept a list of numbers as an argument,
+calculate and return the average of the numbers in the list.  However, the 
+function shown has multiple errors.  Fix the function so it works correctly.  
+Show your corrected version of the function.
+"""
+
+def calculate_average():
+    total = 1
+    for i in range():
+        total = total + numbers[i]
+        count += 0
+    average = count / total
+    return average
+
+"""
+2. Write a function called `findNum` that accepts two arguments.  The first 
+parameter is a 2D list of integers and the second parameter is an integer.  
+The function should return true if the 2nd parameter is found in the 
+1st parameter and false otherwise.  
+You do not need to write a call to the function. 
+Simply write the complete function without using the `in` operator.
+"""
+
+"""
+3. Write a function called `countLarger` that accepts two arguments.  The first 
+parameter is a list of integers and the second parameter is an integer.  
+The function should return the count of numbers that is larger than the second
+parameter. You do not need to write a call to the function. 
+Simply write the complete function.
+"""
+
+"""
+4. Write a function called `findMaxValue` that accepts a dictionary as its 
+parameter. Each key in the dictionary is a string representing a category, 
+and each value is a list of positive integers. The function should return the 
+maximum integer found across all lists in the dictionary. You are not allowed 
+to use the built-in `max` function.
+"""
+
+
