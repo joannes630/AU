b002358db1702239faee94aa51796aabe59253cc
joannes630 <jreyes@aurora.edu>
2024-11-08 10:17:39 -0600

    Updates to Deque

--- CSC2150/Stack Queue Deque PriorityQueue/DequeAsQueue.java
+++ CSC2150/Stack Queue Deque PriorityQueue/DequeAsQueue.java
@@ -1,14 +1,28 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/*
+    Deque key methods:
+        addFirst(element)   --> adds an element to the head/top
+        addLast(element)    --> adds an element to the tail/bottom
+        pollFirst()         --> removes and returns the first element
+        pollLast()          --> removes and returns the last element
+        peekFirst()         --> Returns, but does not remove, the first element
+        peekLast()          --> Returns, but does not remove, the last element
+        size()              --> returns the number of elements in the deque
+        isEmpty()           --> returns true if the deque is empty
+ */
+
 public class DequeAsQueue {
     public static void main(String[] args) {
         Deque<Integer> queue = new ArrayDeque<>();
 
-        queue.offer(1); // Enqueue
-        queue.offer(2);
-        queue.offer(3);
+        queue.addLast(1);
+        queue.addLast(2);
+        queue.addLast(3);
 
-        System.out.println(queue.poll()); // Dequeue (Output: 1)
+        System.out.println(queue.pollFirst()); //Output: 1
+        System.out.println(queue.pollFirst()); //Output: 2
+        System.out.println(queue.pollFirst()); //Output: 3
     }
 }
--- CSC2150/Stack Queue Deque PriorityQueue/DequeAsStack.java
+++ CSC2150/Stack Queue Deque PriorityQueue/DequeAsStack.java
@@ -1,14 +1,28 @@
 import java.util.ArrayDeque;
 import java.util.Deque;
 
+/*
+    Deque key methods:
+        addFirst(element)   --> adds an element to the head/top
+        addLast(element)    --> adds an element to the tail/bottom
+        pollFirst()         --> removes and returns the first element
+        pollLast()          --> removes and returns the last element
+        peekFirst()         --> Returns, but does not remove, the first element
+        peekLast()          --> Returns, but does not remove, the last element
+        size()              --> returns the number of elements in the deque
+        isEmpty()           --> returns true if the deque is empty
+ */
+
 public class DequeAsStack {
     public static void main(String[] args) {
         Deque<Integer> stack = new ArrayDeque<>();
 
-        stack.push(1); // Push
-        stack.push(2);
-        stack.push(3);
+        stack.addFirst(1);
+        stack.addFirst(2);
+        stack.addFirst(3);
 
-        System.out.println(stack.pop()); // Pop (Output: 3)
+        System.out.println(stack.pollFirst()); // Output: 3
+        System.out.println(stack.pollFirst()); // Output: 2
+        System.out.println(stack.pollFirst()); // Output: 1
     }
 }
