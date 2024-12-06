e790eb0f124d621f8fa11d754249cb199197eb06
joannes630 <130481030+joannes630@users.noreply.github.com>
2024-12-06 08:11:37 -0600

    Update BubbleSort.java

--- CSC2150/Search and Sort/BubbleSort.java
+++ CSC2150/Search and Sort/BubbleSort.java
@@ -4,8 +4,9 @@ public class BubbleSort {
 
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
+        int m = n - 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     // Swap arr[j] and arr[j + 1]
                     int temp = arr[j];
@@ -18,7 +19,8 @@ public class BubbleSort {
 
     public static void main(String[] args) {
         int[] arr = {64, 90, 34, 25, 12, 22, 11};
+        System.out.println("Unsorted array: " + Arrays.toString(arr));
         bubbleSort(arr);
-        System.out.println("Sorted array: " + Arrays.toString(arr));
+        System.out.println("Sorted array:   " + Arrays.toString(arr));
     }
 }
