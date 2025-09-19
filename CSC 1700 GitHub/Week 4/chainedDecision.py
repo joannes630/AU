4f880286ea1b8a0f501e3a68b8baeea0c235c38c
joannes630 <130481030+joannes630@users.noreply.github.com>
2025-09-19 12:42:44 -0500

    Week 4

---
+++ CSC 1700 GitHub/Week 4/chainedDecision.py
@@ -0,0 +1,29 @@
+# Chained decision is another way to implement
+# range checking, without using logical operator
+
+# age = 21
+# Check if a person is between 10 and 20 years old
+# if age >= 10 and age <= 20:
+#     print("In range")
+# else:
+#     print("Out of range")
+
+# Using chained decision
+# if 10 <= age <= 20:
+#     print("In range")
+# else:
+#     print("Out of range")
+
+# Another example
+# temp = 80
+# Check if temperature is within comfortable room range (68–75 F)
+# if temp >= 68 and temp <= 75:
+#     print("Comfortable")
+# else:
+#     print("Uncomfortable")
+
+# Using chained decision
+# if 68 <= temp <= 75:
+#     print("Comfortable")
+# else:
+#     print("Uncomfortable")
