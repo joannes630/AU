9e5f1d743c7eb132785f92da8f648a4f6264bc43
joannes630 <130481030+joannes630@users.noreply.github.com>
2025-09-22 10:10:40 -0500

    Update SentinelExample.java

--- CYB 2810 GitHub/Week 05/WhileLoop/SentinelExample.java
+++ CYB 2810 GitHub/Week 05/WhileLoop/SentinelExample.java
@@ -1,4 +1,10 @@
 package WhileLoop;
+/*
+Write a Java program that uses a sentinel-controlled loop to add numbers entered by the user. The program should
+repeatedly ask the user to enter integers and keep a running total. The loop should stop when the user enters -1,
+which will serve as the sentinel value. After the loop ends, the program should display the final sum of all numbers
+entered (excluding the sentinel).
+ */
 
 import java.util.Scanner;
 
