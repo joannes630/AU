38194103c65614f911423fed47813bf2197ec139
joannes630 <130481030+joannes630@users.noreply.github.com>
2025-09-23 13:09:45 -0500

    Updated instrumentation file

--- CSC 2450 GitHub/Week 05/Instrumentation/instrumentation.c
+++ CSC 2450 GitHub/Week 05/Instrumentation/instrumentation.c
@@ -9,35 +9,43 @@ long get_time_ms() {
     return (tv.tv_sec * 1000) + (tv.tv_usec / 1000);
 }
 
+long get_time_usec() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return (tv.tv_sec * 1000000) + (tv.tv_usec);
+}
+
+
+
 int main() {
     long start, end;
 
     // 1. Measure CPU computation (fast)
-    start = get_time_ms();
-    long sum = 0;
-    for (long i = 0; i < 100_000; i++) {  // 100 million additions
+    start = get_time_usec();
+    volatile long sum = 0;
+    for (long i = 0; i < 100000; i++) {  // 100 thousand additions
         sum += i;
     }
-    end = get_time_ms();
+    end = get_time_usec();
     printf("Computation finished. Sum = %ld\n", sum);
-    printf("Time taken (computation): %ld ms\n\n", end - start);
+    printf("Time taken (computation): %ld microsecond\n\n", end - start);
 
     // 2. Measure I/O operations (slow)
-    start = get_time_ms();
+    start = get_time_usec();
     FILE *fp = fopen("output.txt", "w");
     if (fp == NULL) {
         perror("File open failed");
         return 1;
     }
 
-    for (int i = 0; i < 100000; i++) {  // 100,000 writes
+    for (int i = 0; i < 1000; i++) {  // 100,000 writes
         fprintf(fp, "This is line %d\n", i);
     }
     fclose(fp);
-    end = get_time_ms();
+    end = get_time_usec();
 
     printf("File writing finished.\n");
-    printf("Time taken (I/O): %ld ms\n", end - start);
+    printf("Time taken (I/O): %ld microsecond\n", end - start);
 
     return 0;
 }
