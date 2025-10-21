5df66e018c478a5d9a2219c2ba8266bc586c7797
joannes630 <130481030+joannes630@users.noreply.github.com>
2025-10-21 16:14:50 -0500

    Create usingIndex.py

---
+++ CSC 1700 GitHub/Week 9/Lists/usingIndex.py
@@ -0,0 +1,10 @@
+fruits = ["apple", "banana", "pear", "cherry"]
+
+# Access the first element 
+print(fruits[0])  # Output: "apple" 
+# Access the second element 
+print(fruits[1])  # Output: "banana"
+# Access the third element
+print(fruits[2])  # Output: "pear"
+# Access the fourth element
+print(fruits[3])  # Output: "cherry"
