10045589f932ec6ba60745deccb96650c4937193
joannes630 <130481030+joannes630@users.noreply.github.com>
2025-10-21 16:00:01 -0500

    Update divideByZero.py

--- CSC 1700 GitHub/Week 9/divideByZero.py
+++ CSC 1700 GitHub/Week 9/divideByZero.py
@@ -1,1 +1,9 @@
+x = int(input("Enter x: "))
+y = int(input("Enter y: "))   # Enter 0
+try:
+    answer = x / y
+    print(answer)
+except ZeroDivisionError:
+    print("Cannot divide by zero")
 
+print("Program continues ...")
