a308e67142f20c253a7a0f887262cbbd9ce83da6
joannes630 <130481030+joannes630@users.noreply.github.com>
2025-10-22 10:25:56 -0500

    Create 06_modifyAnElement.py

---
+++ CSC 1700 GitHub/Week 9/Lists/06_modifyAnElement.py
@@ -0,0 +1,6 @@
+fruits = ["apple", "banana", "cherry"]
+print(fruits)  # Output ["apple", "banana", "cherry"]
+
+# Change the second element
+fruits[1] = "blueberry"
+print(fruits)  # Output: ["apple", "blueberry", "cherry"]
