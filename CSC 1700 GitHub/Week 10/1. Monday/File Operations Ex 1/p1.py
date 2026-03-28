3452c642f94e3285ac41c6fb13bd781941adfaf3
joannes630 <130481030+joannes630@users.noreply.github.com>
2026-03-27 20:24:43 -0500

    Update p1.py

--- CSC 1700 GitHub/Week 10/1. Monday/File Operations Ex 1/p1.py
+++ CSC 1700 GitHub/Week 10/1. Monday/File Operations Ex 1/p1.py
@@ -1,5 +1,7 @@
 """
-Write a Python program that opens a file named data.txt, reads the first line from the file, removes any trailing newline characters, and displays the line to the console.
+Write a Python program that opens a file named data.txt, 
+reads the first line from the file, removes any trailing newline 
+characters, and displays the line to the console.
 
 Use the open() and close() functions.
 """
