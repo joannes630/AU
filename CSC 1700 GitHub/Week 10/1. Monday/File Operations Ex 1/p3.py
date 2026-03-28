03de5f2b64ebd67d373d9dcf83e3ad2ad5fbdfaf
joannes630 <130481030+joannes630@users.noreply.github.com>
2026-03-27 20:25:26 -0500

    Update p3.py

--- CSC 1700 GitHub/Week 10/1. Monday/File Operations Ex 1/p3.py
+++ CSC 1700 GitHub/Week 10/1. Monday/File Operations Ex 1/p3.py
@@ -1,4 +1,6 @@
 """
-Write a Python function read_file(filename) that opens a file with the given filename, reads all lines one by one, removes any trailing whitespace or newline characters, and prints each line to the console.
+Write a Python function read_file(filename) that opens a file with the 
+given filename, reads all lines one by one, removes any trailing whitespace 
+or newline characters, and prints each line to the console.
 
 """
