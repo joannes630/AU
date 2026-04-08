a04cf9fec96a621167fa88a504fa2c34d12209d8
joannes630 <130481030+joannes630@users.noreply.github.com>
2026-04-08 10:02:59 -0500

    Create class code.py

---
+++ CSC 1700 GitHub/Week 13/Dictionary/class code.py
@@ -0,0 +1,12 @@
+phonebook = {
+    "Chris": "555-1111",
+    "Katie": "555-2222",
+    "John": "555-3333"
+}
+
+print(phonebook["Joannes"])   # 555-1111
+
+phonebook["Sara"] = "555-4444"     # add new entry
+phonebook["Chris"] = "555-9999"    # update existing entry
+
+
